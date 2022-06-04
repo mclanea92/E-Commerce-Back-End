54cb5225ff8b7fef462533def2d035e5c7d97fae
Adam McLane <mclanea92@gmail.com>
2022-06-04 12:59:26 -0400

    added posts

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -18,14 +18,39 @@ router.get('/', (req, res) => {
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  await Category.findByPk(req.params.id, {
+    attributes: ["id", "category_name"],
+    include: [
+      {
+        model: Product,
+        attributes: ["id", "product_name", "price", "stock", "category_id"],
+      }
+    ]
+  })
+  .then((category) => {
+    res.json(category);
+  })
+  .catch((err) => {
+    res.json(err);
+  });
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new category
+  await Category.create(req.body)
+  .then((newCategory) => 
+  res.status(200).json(newCategory))
+
+  .catch((err) => {
+    console.log(err);
+    res.status(400).json(err)
+  })
 });
+
+
 
 router.put('/:id', (req, res) => {
   // update a category by its `id` value
