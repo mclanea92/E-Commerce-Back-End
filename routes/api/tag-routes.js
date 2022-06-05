b3f666cad713393b993cc30e7cd4ccf5cce911b0
Adam McLane <mclanea92@gmail.com>
2022-06-05 10:33:40 -0400

    added tag routes

--- routes/api/tag-routes.js
+++ routes/api/tag-routes.js
@@ -23,21 +23,68 @@ router.get('/', async (req, res) => {
   })
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find a single tag by its `id`
   // be sure to include its associated Product data
+  await Tag.findByPk(req.params.id, {
+    include: [{
+      model: Product,
+      attributes: ["id", "product_name", "price", "stock", "category_id"],
+      through: "ProductTag"
+    }]
+  })
+  .then((tagID) => {
+    res.json(tagID);
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
-router.post('/', (req, res) => {
+router.post('/', async (req, res) => {
   // create a new tag
+  await Tag.create({
+    tag_name: req.body.tag_name,
+  })
+  .then((tagNew) => {
+    res.json(tagNew);
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
-router.put('/:id', (req, res) => {
+router.put('/:id', async (req, res) => {
   // update a tag's name by its `id` value
+  await Tag.update({
+    tag_name: req.body.tag_name,
+  },
+  {
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tagUpdate) => {
+    res.json(tagUpdate);
+  })
+  .catch((err) => {
+    res.json(err)
+  });
 });
 
-router.delete('/:id', (req, res) => {
+router.delete('/:id', async (req, res) => {
   // delete on tag by its `id` value
+  await Tag.destroy({
+    where: {
+      id: req.params.id,
+    },
+  })
+  .then((tagRemoved) => {
+    res.json(tagRemoved)
+  })
+  .catch((err) => {
+    res.json(err)
+  })
 });
 
 module.exports = router;
